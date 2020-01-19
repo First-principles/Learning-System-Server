8b7a760c84425fc7448baddb6bf9f1677622e200
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-19 23:46:13 +0200

    update

--- routes/api/lessons.js
+++ routes/api/lessons.js
@@ -1,18 +1,10 @@
 var router = require('express').Router();
-var mongoose = require('mongoose');
-var Lesson = mongoose.model('Lesson');
-//var User = mongoose.model('User');
 const { RouteNames } = require("../../constants/constants");
+var mongoose = require('mongoose');
+const addlesson = require('../methods/lessons');
 
-router.post(RouteNames.AddLesson, (req, res, next) => {
-    try {
-        const lesson = new Lesson(req.body.lesson);
-        lesson.save();
-        res.status(202).send({ lesson });
-    } catch (e) {
-        res.status(400).send({ error: { message: "couldn't save lesson" } });
-    }
-});
+
+router.post(RouteNames.AddLesson, addlesson);
 
 
 
