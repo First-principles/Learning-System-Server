5ecbd9f5a2827f0c9c639b380d2ddc09e6738c1b
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-23 21:50:47 +0200

    update user

--- routes/methods/lessons.js
+++ routes/methods/lessons.js
@@ -6,17 +6,22 @@ var Course = mongoose.model('Course');
 const addlesson = (req, res, next) => {
     const LessonInfo = req.body.lesson;
     const CourseInfo = req.body.course;
-    const lesson = new Lesson(LessonInfo);
 
     Course.findOne(CourseInfo)
         .then(course => {
+            if (!course) {
+                res.status(422).send({ errors: { message: "Course not found" } });
+            }
+            const lesson = new Lesson(LessonInfo);
+
             course.lessons.push(lesson);
+            lesson.save().then(() => {
+                res.status(202).send({ lesson });
+            }).catch(next);
+
         }).catch(() => {
             res.status(422).send({ errors: { message: "Course not found" } });
         });
-    lesson.save().then(() => {
-        res.status(202).send({ lesson });
-    }).catch(next);
 };
 
 module.exports = addlesson;
