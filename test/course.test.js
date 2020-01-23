182440688009ec215768359bc8d988911f2224db
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-23 20:27:01 +0200

    added course test

--- test/course.test.js
+++ test/course.test.js
@@ -20,4 +20,20 @@ describe('Courses Tests', () => {
                 done();
             });
     });
+
+    it("Create Course while autorized", (done) => {
+        request(app)
+            .post(RouteNames.AddCourse)
+            .send(config.Course)
+            .set("Accept", "application/json")
+            .set('authorization', config.AuthToken)
+            .end(function(err, response) {
+                if (err) {
+                    return err;
+                }
+                //console.log(response.body);
+                expect(response.statusCode).to.equal(202);
+                done();
+            });
+    });
 });
