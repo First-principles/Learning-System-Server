8b4ae491983fe1b664eee5c901409d211dd02a59
ibrahimshamma99 <i.abushammah@gmail.com>
2020-02-01 14:59:10 +0200

    connect course component

--- Course/test/course.test.js
+++ Course/test/course.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../../server/server");
 const { RouteNames } = require("../../constants/constants");
 var expect = require('chai').expect;
-const config = require("../config/constants");
+const config = require("../../config/constants");
 
 describe('Courses Tests', () => {
 
--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const app = require("./intialize");
 const mongoose = require("mongoose");
 const config = require('../config/config');
 var isProduction = process.env.NODE_ENV === 'production';
+//SECTION Importing components
 var userComponent = require("../User/index");
+var courseComponent = require("../Course/index");
 
 //NOTE Import API
 app.use(require('../routes'));
 app.use(userComponent);
+app.use(courseComponent);
 
 if (isProduction) {
     mongoose.connect(process.env.MONGODB_URI, {
