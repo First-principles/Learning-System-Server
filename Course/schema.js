aa242e34eee5d07983d2209708949b3c4e151357
ibrahimshamma99 <i.abushammah@gmail.com>
2020-02-28 19:31:31 +0200

    update

--- Course/schema.js
+++ Course/schema.js
@@ -1,9 +1,17 @@
 var mongoose = require('mongoose');
-
+/**
+ * @title ===   => Course title
+ * @author == ID => Course's Author
+ * @description => A brief about the course
+ * @images => images to be viewed in the course intro
+ * @favoritesCount => people whole like this course
+ * @lessons => lessons being contained in the course
+ * @comments => Course's comments
+ */
 var CourseSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: String,
-    images: Buffer,
+    images: [Buffer],
     favoritesCount: { type: Number, default: 0 },
     lessons: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lesson' }],
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
