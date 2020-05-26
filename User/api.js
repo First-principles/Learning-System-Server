2d9292445f743dd10aeba5d26efd3598f8a2a3cd
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-26 10:53:34 +0300

    👌 IMPROVE:

--- User/api.js
+++ User/api.js
@@ -1,26 +1,23 @@
-var router = require('express').Router();
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
-const { adduser, login , updateUser ,followUser ,addAdmin} = require('./methods');
-// const {params , userPopulation} = require('./populations');
+var router = require("express").Router();
+var mongoose = require("mongoose");
+var User = mongoose.model("User");
+const Controlers = require("./methods");
+const parameters = require("./populations");
 const Routes = require("./constants");
 const helper = require("../helper/helper");
-
+const params = parameters.params;
 // router.param(params.user,userPopulation);
 
 //SECTION add user
-router.post(Routes.AddUser, adduser);
+router.post(Routes.AddUser, Controlers.register);
 
-//SECTION user login 
-router.post(Routes.Login, login);
+//SECTION user login
+router.post(Routes.Login, Controlers.login);
 
 //SECTION update user
-router.put(Routes.update , helper.required , updateUser);
+router.put(Routes.update, helper.required, Controlers.update);
 
 //SECTION follow user
-router.post(Routes.follow , helper.required , followUser);
-
-//SECTION add superuser
-router.post(Routes.admin , helper.required , addAdmin);
+router.post(Routes.follow, helper.required, Controlers.followUser);
 
 module.exports = router;
--- User/methods.js
+++ User/methods.js
@@ -1,126 +1,144 @@
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
+var mongoose = require("mongoose");
+var User = mongoose.model("User");
 
 //TODO encapsulate messages
-const adduser = (req, res, next) => {
-    var user = new User();
-    const UserInfo = req.body.user;
-    try {
-        user.username = UserInfo.username;
-        user.first_name = UserInfo.first_name;
-        user.last_name = UserInfo.last_name;
-        user.email = UserInfo.email;
-        user.setPassword(UserInfo.password);
-        user.token = user.generateJWT();
-    } catch (e) {
-        return res.status(422).send({
-            error:{message:"error during registeration"}
-        });
-    }
+const register = (req, res, next) => {
+  var user = new User();
+  const UserInfo = req.body.user;
+  try {
+    user.username = UserInfo.username;
+    user.first_name = UserInfo.first_name;
+    user.last_name = UserInfo.last_name;
+    user.email = UserInfo.email;
+    user.setPassword(UserInfo.password);
+    user.token = user.generateJWT();
+  } catch (e) {
+    return res.status(422).send({
+      error: { message: "error during registeration" },
+    });
+  }
 
-    user.save(function(err) {
-        if (err) {
-            if (err.name === 'MongoError' && err.code === 11000) {
-                return res.status(422).send({ succes: false, message: 'User already exist!' });
-            }
-            return res.status(422).send(err);
-        }
-        res.status(202).json({
-            user:user.toAuthJSON()
-        });
+  user.save(function (err) {
+    if (err) {
+      if (err.name === "MongoError" && err.code === 11000) {
+        return res
+          .status(422)
+          .send({ succes: false, message: "User already exist!" });
+      }
+      return res.status(422).send(err);
+    }
+    res.status(202).json({
+      user: user.toAuthJSON(),
+    });
+  });
+};
+
+const addAdmin = () => {
+  var user = new User();
+  const UserInfo = req.body.user;
+  try {
+    user.username = UserInfo.username;
+    user.role = "admin";
+    user.first_name = UserInfo.first_name;
+    user.last_name = UserInfo.last_name;
+    user.email = UserInfo.email;
+    user.setPassword(UserInfo.password);
+    user.token = user.generateJWT();
+  } catch (e) {
+    return res.status(422).send({
+      error: { message: "error during registeration" },
+    });
+  }
+
+  user.save(function (err) {
+    if (err) {
+      if (err.name === "MongoError" && err.code === 11000) {
+        return res
+          .status(422)
+          .send({ succes: false, message: "User already exist!" });
+      }
+      return res.status(422).send(err);
+    }
+    res.status(202).json(user.toAuthJSON());
+  });
+};
+
+const login = async (req, res, next) => {
+  const UserInfo = req.body.user;
+  if (!UserInfo.email) {
+    res.send(422).json({ error: { message: "please provide email " } });
+  }
+  if (!UserInfo.password) {
+    return res.status(422).json({ errors: { password: "can't be blank" } });
+  }
+  await User.findOne({ email: UserInfo.email })
+    .then((user) => {
+      if (user.validPassword(UserInfo.password)) {
+        return res.status(202).json(user.toAuthJSON());
+      } else {
+        return res
+          .status(422)
+          .send({ errors: { authentication: "authentication error" } });
+      }
+    })
+    .catch(() => {
+      return res
+        .status(422)
+        .send({ errors: { authentication: "Email not valid" } });
     });
 };
 
-const addAdmin = () => {    var user = new User();
-    const UserInfo = req.body.user;
-    try {
-        user.username = UserInfo.username;
-        user.role = 'admin';
-        user.first_name = UserInfo.first_name;
-        user.last_name = UserInfo.last_name;
-        user.email = UserInfo.email;
-        user.setPassword(UserInfo.password);
-        user.token = user.generateJWT();
-    } catch (e) {
-        return res.status(422).send({
-            error:{message:"error during registeration"}
-        });
-    }
-
-    user.save(function(err) {
-        if (err) {
-            if (err.name === 'MongoError' && err.code === 11000) {
-                return res.status(422).send({ succes: false, message: 'User already exist!' });
-            }
-            return res.status(422).send(err);
-        }
-        res.status(202).json(user.toAuthJSON());
+const update = (req, res, next) => {
+  const updateData = req.body.user;
+  if (!updateData) {
+    res.status(422).send({ message: "please provide what you want to update" });
+  }
+  User.findById(updateData._id)
+    .then(function (user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+      user.handleInfo(updateData);
+      return user.save().then(function () {
+        return res.json({ user: user.toAuthJSON() });
+      });
+    })
+    .catch(() => {
+      res.status(422).send({ message: "couldn't update user" });
     });
-}
+};
+const followUser = (req, res, next) => {
+  const userInfo = req.body.user;
+  const followedInfo = req.body.followed;
+  if (!userInfo) {
+    res.status(422).send({ message: "User not provided" });
+  }
+  if (!followedInfo) {
+    res.status(422).send({ message: "Followed not provided" });
+  }
+  User.findById(userInfo._id)
+    .then((user) => {
+      User.findById(followedInfo._id)
+        .then((followed) => {
+          if (!user) {
+            res.status(422).send({ message: "User not found" });
+          }
+          if (!followed) {
+            res.status(422).send({ message: "followed not found" });
+          }
 
-const login = async(req, res, next) => {
-    const UserInfo = req.body.user;
-    if (!UserInfo.email) {
-        res.send(422).json({ error: { message: "please provide email " } });
-    };
-    if (!UserInfo.password) {
-        return res.status(422).json({ errors: { password: "can't be blank" } });
-    };
-    await User.findOne({ email: UserInfo.email }).then(
-        user => {
-            if (user.validPassword(UserInfo.password)) {
-                return res.status(202).json(
-                    user.toAuthJSON()
-                )
-            } else {
-                return res.status(422).send({ errors: { authentication: "authentication error" } })
-            }
-        }
-    ).catch(
-        () => {
-            return res.status(422)
-                .send({ errors: { authentication: "Email not valid" } })
-        }
-    );
+          user.following.push(followed);
+          followed.followedBy.push(user);
+          user.save();
+          followed.save();
+
+          return res.status(202).json(user.toAuthJSON());
+        })
+        .catch(next);
+    })
+    .catch(next);
 };
 
-const updateUser = (req ,res, next)=>{
-        const updateData = req.body.user;
-        if (!updateData){
-            res.status(422).send({"message":"please provide what you want to update"})
-        }
-        User.findById(updateData._id).then(function(user) {
-            if (!user) { return res.sendStatus(401); }
-            user.handleInfo(updateData);
-            return user.save()
-                .then(function() {
-                    return res.json({ user: user.toAuthJSON() });
-                });
-        }).catch(()=>{
-            res.status(422).send({"message":"couldn't update user"})
-        }
-        );
-    };
-const followUser = (req,res,next)=>{
-    const userInfo = req.body.user;
-    const followedInfo = req.body.followed;
-    if (!userInfo){res.status(422).send({"message":"User not provided"})};
-    if (!followedInfo){res.status(422).send({"message":"Followed not provided"})};
-    User.findById(userInfo._id).then((user)=>{
-        User.findById(followedInfo._id).then((followed)=>{
-            if (!user){res.status(422).send({"message":"User not found"})}
-            if (!followed){res.status(422).send({"message":"followed not found"})}
-            
-            user.following.push(followed);
-            followed.followedBy.push(user);
-            user.save();
-            followed.save();
-            
-            return res.status(202).json(
-                user.toAuthJSON()
-            )
-        }).catch(next)
-    }).catch(next)
-}
+const userControler = { register, login, update , followUser};
 
-module.exports = { adduser, login  , updateUser , followUser , addAdmin};
+module.exports = userControler;
